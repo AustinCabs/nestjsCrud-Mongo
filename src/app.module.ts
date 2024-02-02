e8d7c5ca863a70b4e18aaeb036f36b1631f59eaa
AustinCabs <austinbrien.cabanada@gmail.com>
2024-02-03 03:37:39 +0800

    added gphql todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { join } from 'path';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongoadmin:12345@localhost:27017/'), TodoModule],
+  imports: [MongooseModule.forRoot('mongodb://mongoadmin:12345@localhost:27017/'),
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true
+  }),
+    , TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
